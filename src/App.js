1504d2f8edd6d738d3e51ccfd7c50a0100985428
Jitendra Garg <58606315+Jitendra3018@users.noreply.github.com>
2022-01-28 16:38:44 +0530

    All the Routing, Components and Pages Added

--- src/App.js
+++ src/App.js
@@ -1,15 +1,32 @@
 import styled from "styled-components";
+import Home from "./Pages/Home";
 import SideBar from "./components/SideBar";
 import TopBar from "./components/TopBar";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import UserList from "./Pages/UserList";
+import User from "./Pages/User";
+import NewUser from "./Pages/NewUser";
+import ProductList from "./Pages/ProductList";
+import Product from "./Pages/Product";
+import NewProduct from "./Pages/NewProduct";
 
 function App() {
 	return (
-		<div>
+		<Router>
 			<TopBar />
 			<Container>
 				<SideBar />
+				<Routes>
+					<Route exact path="/" element={<Home />} />
+					<Route path="/users" element={<UserList />} />
+					<Route path="/user/:userId" element={<User />} />
+					<Route path="/newUser" element={<NewUser />} />
+					<Route path="/products" element={<ProductList />} />
+					<Route path="/product/:productId" element={<Product />} />
+					<Route path="/newProduct" element={<NewProduct />} />
+				</Routes>
 			</Container>
-		</div>
+		</Router>
 	);
 }
 
@@ -17,4 +34,5 @@ export default App;
 
 const Container = styled.div`
 	display: flex;
+	margin-top: 10px;
 `;
